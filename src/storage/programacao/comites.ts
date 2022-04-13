ab9f6500d11e44630506a8e4d973c220c1eb4159
Walcy Rios <wjuniorrios@gmail.com>
2022-04-13 15:19:26 -0300

    alteração no comitê

---
+++ src/storage/programacao/comites.ts
@@ -0,0 +1,181 @@
+export const ComiteListOrganization = 
+  [
+    {
+      coordenador: 'ANA FLÁVIA MARINHO DE LIMA GARROTE',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'ANDRÉ LUIZ ALVES',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'CARMEN CECÍLIA CENTENO',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'FELIPE DE SOUSA NOBRE',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'FREDERICO ANDRÉ RABELO',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'GENILDA DA SILVA ALEXANDRIA SOUSA',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'JORIVER RODRIGUES CANEDO',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'LUCIANA CASALETTI',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'LUDMILLA REIS PINHEIRO DOS SANTOS',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'LUIZ ÁLVARO DE OLIVEIRA JÚNIOR',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'MARTHA NASCIMENTO CASTRO',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'MÍRIAN SANDRA ROSA GUSMÃO',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'RODRIGO MARTINEZ CASTRO',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'SALMEN CHAQUIP BUKZEM',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'WANDERSON RAINER HILARIO DE ARAUJO',
+      area: 'Coordenador da IV JCECEC'
+    },
+  ]
+
+export const ComiteListCientific = 
+  [
+    {
+      coordenador: 'Prof. Dr. Clarimar José Coelho',
+      area: 'Coordenador da IV JCECEC'
+    },
+    {
+      coordenador: 'Prof. Dr. Arlindo Rodrigues Galvão Filho',
+      area: 'Doutor em Ciência da Computação - UFG',
+      // participantes: [
+      //   'Coordenadora: Profa. Dra. Carmen Cecília Centeno - PUC Goiás',
+      //   'Prof. Dr. Adelino Candido Pimenta - PUC Goiás',
+      //   'Prof. Dr. Anderson da Silva Soares – UFG',
+      //   'Prof. Dr. Antônio Newton Borges – PUC Goiás',
+      //   'Prof. Dr. Arlindo Rodrigues Galvão Filho – PUC Goiás',
+      //   'Prof. Dr. Cloves Gonçalves Rodrigues – PUC Goiás',
+      //   'Prof. Dr. Danns Pereira Barbosa – PUC Goiás',
+      //   'Prof. Dr. Duelci Aparecido de Freitas Vaz – PUC Goiás',
+      //   'Prof. Dr. Eduardo Noronha de Andrade Freitas – IFG',
+      //   'Prof. Dr. Gustavo Teodoro Laureano - UFG',
+      //   'Prof. Dr. Marcos Lajovic Carneiro – PUC Goiás',
+      //   'Profa. Dra. Michelle Pereira Quintino – PUC Goiás',
+      //   'Prof. Dr. Rafael Viana de Carvalho – PUC Goiás',
+      //   'Prof. Dr. Raffael Costa Figueiredo Pinto - PUC Goiás',
+      //   'Prof. Dr. Sibelius Lellis Vieira - PUC Goiás',
+      //   'Prof. Dr. Sirlon Diniz de Carvalho – IFG - Goiânia',
+      //   'Profa. Dra. Solange da Silva - PUC Goiás',
+      //   'Prof. Dr. Ulisses Rodrigues Afonseca – IFG-Luziânia'
+      // ]
+    },
+    {
+      coordenador: 'Prof. Dr. Clóves Gonçalves Rodrigues',
+      area: 'Coordenador de postêres',
+      // participantes: [
+      //   'Coordenador: Prof. Dr. Arlindo Rodrigues Galvão Filho - PUC Goiás',
+      //   'Prof. Dr. Raffael Costa Figueiredo Pinto - PUC Goiás',
+      //   'Prof. Dr. Anderson Costa da Silva – PUC Goiás',
+      //   'Prof. Dr. Danns Pereira Barbosa – PUC Goiás',
+      //   'Prof. Dr. Vicente Paulo de Camargo – PUC Goiás',
+      //   'Prof. Dr. Arlindo Rodrigues Galvão Filho – PUC Goiás',
+      //   'Prof. Dr. Rafael Viana de Carvalho – PUC Goiás',
+      //   'Prof. Ms. Joriver Rodrigues Canedo – PUC Goiás',
+      //   'Profa. Ma. Lucilia Gomes Ribeiro – PUC Goiás',
+      //   'Profa. Ma. Ludmilla Reis Pinheiro dos Santos – PUC Goiás',
+      //   'Profa. Ma. Mirian Sandra Rosa Gusmão – PUC Goiás',
+      //   'Prof. Ms. Gustavo Siqueira Vinhal – PUC Goiás',
+      //   'Prof. Esp. Gildenor de Souza Amorim Cavalcante – PUC Goiás',
+      //   'Prof. Msc. Adriana dos Reis Ferreira – IFG-Goânia',
+      //   'Prof. Msc. Christiane Borges Santos – IFG – Luziânia',
+      //   'Prof. Esp. Viviane Bessa Ferreira – IFG',
+      //   'Prof. Msc. Carina Calixto Ribeiro de Araujo - IFG-Goiânia',
+      //   'Estudante Douglas Vieira do Nascimento – PUC Goiás',
+      //   'Estudante Rone Cesar Brandão – PUC Goiás'
+      // ]
+    },    
+    {
+      coordenador: 'Felipe Correa Veloso Dos Santos',
+      area: 'XXXXXXXXXXX',
+      // participantes: [
+      //   'Centro Acadêmico de Física',
+      //   'Centro Acadêmico de Matemática',
+      //   'Centro Acadêmico de Química',
+      //   'Centro Acadêmico de Ciência da Computação',
+      //   'Centro Acadêmico de Engenharia de Computação',
+      //   'Centro Acadêmico de Desenvolvimento de Sistemas'
+      // ]
+    },
+    {
+      coordenador: 'José Elmo De Menezes',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'José Luiz De Freitas Júnior',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Ludmilla Reis Pinheiro Dos Santos',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Luis Fernando Pagotti',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Marcos Lajovic Carneiro',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Maria José Pereira Dantas',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Martha Nascimento Castro',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Pedro Henrique Maximo Pereira',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Ricardo Luiz Machado',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Roberto Marcio Dos Santos',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Solange Da Silva',
+      area: 'Coordenador de postêres'
+    },
+    {
+      coordenador: 'Tai Hsuan An',
+      area: 'Coordenador de postêres'
+    },
+  ]
+
